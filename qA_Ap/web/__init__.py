584436028ad5c09cf2dfff2642a743f18e4d6943
Martin Ashton-Lomax <ashtonlomaxmartin@gmail.com>
2025-08-31 21:36:32 +0200

    fix custom frontend input

--- qA_Ap/web/__init__.py
+++ qA_Ap/web/__init__.py
@@ -2,14 +2,18 @@ from .api import server
 from bottle import static_file
 from pathlib import Path
 
-def activate_integrated_frontend(custom_path:Path = None):
+def activate_integrated_frontend(custom_path:str = None):
 
-    front_path = Path(__file__).parent.resolve() if custom_path == None else custom_path
+    if custom_path == None:
+        front_path = Path(__file__).parent.resolve()
+        front_path = Path(front_path,"frontend")
+    else:
+        front_path = Path(custom_path).resolve()
 
     @server.get("/")
     def frontend():
-        return static_file("index.html", root=Path(front_path,"frontend"))
+        return static_file("index.html", root=front_path)
     
     @server.get('/static/<filepath:path>')
     def server_static(filepath):
-        return static_file(filepath, root=Path(front_path,"frontend"))
+        return static_file(filepath, root=front_path)
